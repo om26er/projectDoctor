0c319448fd5e999b76f581b0c7bd75ef4f05fcc3
s9iper1 <s9iper1@gmail.com>
2017-02-20 16:08:29 +0500

    work

--- app/src/main/java/com/byteshaft/doctor/MainActivity.java
+++ app/src/main/java/com/byteshaft/doctor/MainActivity.java
@@ -1,9 +1,6 @@
 package com.byteshaft.doctor;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -30,12 +27,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        Menu menu = navigationView.getMenu();
-        menu.add("Search Doctor").setIcon(android.R.drawable.ic_search_category_default);
-        menu.add("My Appointments");
-        menu.add("Profile");
-
-        navigationView.invalidate();
+        navigationView.inflateMenu(R.menu.patient_menu);
     }
 
     @Override
@@ -76,9 +68,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_search) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_appointment) {
 
         } else if (id == R.id.nav_slideshow) {
 
