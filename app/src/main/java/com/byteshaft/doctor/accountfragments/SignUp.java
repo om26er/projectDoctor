018c98d8028da1205eb1f7b41acd65c13cf78a57
hsunain hussi <husnain446@gmail.com>
2017-03-01 17:44:27 +0500

    implement functionalities

--- app/src/main/java/com/byteshaft/doctor/accountfragments/SignUp.java
+++ app/src/main/java/com/byteshaft/doctor/accountfragments/SignUp.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.byteshaft.doctor.R;
 
@@ -12,13 +15,43 @@ import com.byteshaft.doctor.R;
  * Created by husnain on 2/20/17.
  */
 
-public class SignUp extends Fragment {
+public class SignUp extends Fragment implements View.OnClickListener {
 
     private View mBaseView;
+
+    private EditText mUserName;
+    private EditText mEmail;
+    private EditText mPassword;
+    private EditText mVerifyPassword;
+
+    private Button mLoginButton;
+
+    private TextView mForgotPasswordTextView;
+    private TextView mSignUpTextView;
+
+    private String mPasswordString;
+    private String mUserNameString;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mBaseView = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        mUserName = (EditText) mBaseView.findViewById(R.id.username_edit_text);
+        mEmail = (EditText) mBaseView.findViewById(R.id.email_edit_text);
+        mPassword = (EditText) mBaseView.findViewById(R.id.password_edit_text);
+        mVerifyPassword = (EditText) mBaseView.findViewById(R.id.verify_password_edit_text);
+        mLoginButton = (Button) mBaseView.findViewById(R.id.button_login);
+        mForgotPasswordTextView = (TextView) mBaseView.findViewById(R.id.forgot_password_text_view);
+        mSignUpTextView = (TextView) mBaseView.findViewById(R.id.sign_up_text_view);
+
+        mLoginButton.setOnClickListener(this);
+        mForgotPasswordTextView.setOnClickListener(this);
+        mSignUpTextView.setOnClickListener(this);
         return mBaseView;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
