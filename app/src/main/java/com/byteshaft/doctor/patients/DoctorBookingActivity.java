b3e27490be50538d2a7d4f2214e4f3a800a83277
s9iper1 <s9iper1@gmail.com>
2017-03-08 22:13:47 +0500

    fix conflicts commented some code by @shahid

--- app/src/main/java/com/byteshaft/doctor/patients/DoctorBookingActivity.java
+++ app/src/main/java/com/byteshaft/doctor/patients/DoctorBookingActivity.java
@@ -2,27 +2,20 @@ package com.byteshaft.doctor.patients;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-<<<<<<< HEAD
-import android.widget.Toast;
-=======
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.TextClock;
 import android.widget.TextView;
->>>>>>> upstream/master
+import android.widget.Toast;
 
-import com.byteshaft.doctor.MainActivity;
 import com.byteshaft.doctor.R;
 
-<<<<<<< HEAD
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
 
-public class DoctorBookingActivity extends AppCompatActivity {
-=======
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class DoctorBookingActivity extends AppCompatActivity implements View.OnClickListener {
@@ -35,13 +28,11 @@ public class DoctorBookingActivity extends AppCompatActivity implements View.OnC
     private ImageButton mCallButton;
     private ImageButton mChatButton;
     private ImageButton mFavButton;
->>>>>>> upstream/master
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_booking);
-<<<<<<< HEAD
         HashSet<Date> events = new HashSet<>();
         events.add(new Date());
 
@@ -59,33 +50,31 @@ public class DoctorBookingActivity extends AppCompatActivity implements View.OnC
                 Toast.makeText(DoctorBookingActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
             }
         });
-=======
-        mDoctorName = (TextView) findViewById(R.id.doctor_name_booking);
-        mDoctorSpeciality = (TextView) findViewById(R.id.doctor_sp_booking);
-        mDoctorRating = (RatingBar) findViewById(R.id.doctor_rating_booking);
-        mtime = (TextClock) findViewById(R.id.clock);
-        mDoctorImage = (CircleImageView) findViewById(R.id.doctor_image_booking);
-        mCallButton = (ImageButton) findViewById(R.id.call_button_booking);
-        mChatButton = (ImageButton) findViewById(R.id.chat_button_booking);
-        mFavButton = (ImageButton) findViewById(R.id.button_fav_booking);
-        mCallButton.setOnClickListener(this);
-        mChatButton.setOnClickListener(this);
-        mFavButton.setOnClickListener(this);
+//        mDoctorName = (TextView) findViewById(R.id.doctor_name_booking);
+//        mDoctorSpeciality = (TextView) findViewById(R.id.doctor_sp_booking);
+//        mDoctorRating = (RatingBar) findViewById(R.id.doctor_rating_booking);
+//        mtime = (TextClock) findViewById(R.id.clock);
+//        mDoctorImage = (CircleImageView) findViewById(R.id.doctor_image_booking);
+//        mCallButton = (ImageButton) findViewById(R.id.call_button_booking);
+//        mChatButton = (ImageButton) findViewById(R.id.chat_button_booking);
+//        mFavButton = (ImageButton) findViewById(R.id.button_fav_booking);
+//        mCallButton.setOnClickListener(this);
+//        mChatButton.setOnClickListener(this);
+//        mFavButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.call_button_booking:
-                // TODO: 07/03/2017 call intent
-                break;
-            case R.id.chat_button_booking:
-                // TODO: 07/03/2017 open chat activity
-                break;
-            case R.id.button_fav_booking:
-                mFavButton.setBackgroundResource(R.mipmap.ic_heart_fill);
-                break;
-        }
->>>>>>> upstream/master
+//        switch (view.getId()) {
+//            case R.id.call_button_booking:
+//                // TODO: 07/03/2017 call intent
+//                break;
+//            case R.id.chat_button_booking:
+//                // TODO: 07/03/2017 open chat activity
+//                break;
+//            case R.id.button_fav_booking:
+//                mFavButton.setBackgroundResource(R.mipmap.ic_heart_fill);
+//                break;
+//        }
     }
 }
