0bdcb1008300fac658a73ec7b3ca3a22dbd3bb59
s9iper1 <s9iper1@gmail.com>
2017-03-24 15:19:39 +0500

    work

--- app/src/main/java/com/byteshaft/doctor/patients/PatientDetails.java
+++ app/src/main/java/com/byteshaft/doctor/patients/PatientDetails.java
@@ -1,8 +1,10 @@
 package com.byteshaft.doctor.patients;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -10,7 +12,7 @@ import android.widget.TextView;
 
 import com.byteshaft.doctor.R;
 
-public class PatientDetails extends AppCompatActivity {
+public class PatientDetails extends AppCompatActivity implements View.OnClickListener {
 
     private TextView patientName;
     private TextView patientAge;
@@ -34,7 +36,7 @@ public class PatientDetails extends AppCompatActivity {
         callButton = (ImageButton) findViewById(R.id.call_button_);
         chatButton = (ImageButton) findViewById(R.id.chat_button_);
         appointmentButton = (Button) findViewById(R.id.button_appointment);
-
+        appointmentButton.setOnClickListener(this);
         docId = (EditText) findViewById(R.id.doc_id);
         birthDate = (EditText) findViewById(R.id.birth_date);
         patientAddress = (EditText) findViewById(R.id.patient_address);
@@ -47,6 +49,15 @@ public class PatientDetails extends AppCompatActivity {
                 onBackPressed();
                 return true;
             default: return false;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_appointment:
+                startActivity(new Intent(getApplicationContext(), DoctorBookingActivity.class));
+                break;
         }
     }
 }
--- app/src/main/java/com/byteshaft/doctor/utils/AppGlobals.java
+++ app/src/main/java/com/byteshaft/doctor/utils/AppGlobals.java
@@ -83,7 +83,7 @@ public class AppGlobals extends Application {
 
     public static final String KEY_TOKEN = "token";
     public static final String USER_ACTIVATION_KEY = "activation_key";
-    public static final int LOCATION_ENABLE = 2;
+    public static final int LOCATION_ENABLE = 3;
     public static ImageLoader sImageLoader;
 
     @Override
